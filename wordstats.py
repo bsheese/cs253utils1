9514cc4f3771ad3ae23097366066aa780954a794
Ethan Lowder <elowder@iwu.edu>
2024-02-16 09:15:01 -0600

    added word and char count to wordstats

--- wordstats.py
+++ wordstats.py
@@ -5,3 +5,20 @@ def average_length(user_string):
     else:
         avg = 0
     return avg
+
+
+def word_count(user_string):
+    if user_string:
+        words = len(user_string.split())
+    else:
+        words = 0
+
+    return words
+
+def char_count(user_string):
+    if user_string:
+        chars = len(user_string)
+    else:
+        chars = 0
+
+    return chars
